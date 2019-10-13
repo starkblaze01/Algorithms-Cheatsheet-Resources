5445804d29a2dc14f52160f309bb812905db1124
Anito Anto <49053859+anitoanto@users.noreply.github.com>
2019-10-13 20:58:10 +0530

    Updated code to Standard C++

--- sqrt_function_algorithm/sqrt_cplusplus.cpp
+++ sqrt_function_algorithm/sqrt_cplusplus.cpp
@@ -1,32 +1,35 @@
-#include<iostream.h>
-#include<conio.h>
+#include<iostream>
+using namespace std;
 
-double sq(double n)
-{
+double sqrt_fun(double n) {
+	
 	double beg=0,last=n,mid;
-	while(beg<=last)
-	{
+	
+	while(beg<=last) {
+		
 		mid=(beg+last)/2;
-		if(n>mid*mid)
-		{
-		if(n-mid*mid<=0.000001)return mid;
-		}else
-		{
-		if(mid*mid-n<=0.000001)return mid;
+		
+		if(n>mid*mid) {
+			if(n-mid*mid<=0.000001)
+			return mid;
+		} else {
+			if(mid*mid-n<=0.000001)
+			return mid;
 		}
-		if(n>mid*mid)beg=mid;
+		if(n>mid*mid) beg=mid;
 		else last=mid;
 	}
 	return -1;
 }
-void main()
-{
-	clrscr();
+
+int main() {
+	
 	double n;
-	cout<<"Enter no : ";cin>>n;
-	if(sq(n)!=-1)
-	cout<<"\nSqrt : "<<sq(n);
-	else
-	cout<<"\nERROR !\n";
-	getch();
+	cout<<"Enter the number : ";
+	cin>>n;
+	if(sqrt_fun(n)!=-1) cout<<"\nSquare root of "<<n<<" = "<<sqrt_fun(n)<<endl;
+	else cout<<"\nERROR !"<<endl;
+	
+	system("pause");
+	return 0;
 }
