454473e903551268473a56d3dca4c24bb16f1e29
manit2110 <56434506+manit2110@users.noreply.github.com>
2019-10-16 15:48:24 +0530

    Added folder with python code to calculate mean of input array
    Change innput array according to need.

---
+++ Mean/calMean.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def calMean(array):
+	mean = 0
+	for i in range(len(array)):
+		mean = mean + array[i]
+	mean = mean/float(len(array))
+	return mean
+
+if __name__ == "__main__": 
+	array = [1,2,3,4]
+	mean = calMean(array)
+	print (mean)
