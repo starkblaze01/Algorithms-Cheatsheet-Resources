6ab80aecf14cf633bbfc57c5c19f676a4c9eaccf
Nischal Ashok <41947720+Nish-19@users.noreply.github.com>
2019-10-17 13:59:53 +0530

    Updated ith smallest element code

--- Finding ith smallest element/finding ith smallest element.c
+++ Finding ith smallest element/finding ith smallest element.c
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <stdio.h>
 #include<stdlib.h>
