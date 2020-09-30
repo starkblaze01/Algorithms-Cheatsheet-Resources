f3baaf50bf09dda1fe517174be0fe554649baa09
OjusWiZard <ojuswimail@gmail.com>
2020-10-01 02:05:07 +0530

    Added Bubble Sort

---
+++ cpp/Sorting Algorithms/Bubble Sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void bubbleSort(int *arr, int n)
+{
+	for (int i = 0; i < n; i++)
+		for (int j = i + 1; j < n; j++)
+			swap(arr[i], arr[j]);
+}
+
+int main()
+{
+	int n;
+	cout << "Enter the number of Elements: ";
+	cin >> n;
+	int arr[n];
+	cout << "\nEnter the Array: ";
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	bubbleSort(arr, n);
+
+	cout << "\nThe Array after sorting is: ";
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+}
