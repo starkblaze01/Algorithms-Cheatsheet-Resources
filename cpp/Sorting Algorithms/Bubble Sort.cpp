100cbe34bd87df466fa08eb231dfd3b786409d19
Mayank Pathela <mp.pathela@gmail.com>
2020-10-01 22:59:48 +0530

    Correcting Algorithm (#282)
    It was selection sort before.

--- cpp/Sorting Algorithms/Bubble Sort.cpp
+++ cpp/Sorting Algorithms/Bubble Sort.cpp
@@ -11,9 +11,9 @@ void swap(int &a, int &b)
 void bubbleSort(int *arr, int n)
 {
 	for (int i = 0; i < n; i++)
-		for (int j = i + 1; j < n; j++)
-			if (arr[i] > arr[j])
-				swap(arr[i], arr[j]);
+		for (int j = 0; j < n-i-1; j++)
+			if (arr[j] > arr[j+1])
+				swap(arr[j], arr[j+1]);
 }
 
 int main()
