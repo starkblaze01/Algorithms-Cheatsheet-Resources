648a11387cc4e051ddc3ba37e22375b81e0b5c64
OjusWiZard <ojuswimail@gmail.com>
2020-10-01 20:08:21 +0530

    added swapping condition

--- cpp/Sorting Algorithms/Bubble Sort.cpp
+++ cpp/Sorting Algorithms/Bubble Sort.cpp
@@ -12,7 +12,8 @@ void bubbleSort(int *arr, int n)
 {
 	for (int i = 0; i < n; i++)
 		for (int j = i + 1; j < n; j++)
-			swap(arr[i], arr[j]);
+			if (arr[i] > arr[j])
+				swap(arr[i], arr[j]);
 }
 
 int main()
