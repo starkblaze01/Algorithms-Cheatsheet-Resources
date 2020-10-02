505cf3f8919927e9cf871cbee08dc483893f6c39
Mayank Pathela <mp.pathela@gmail.com>
2020-10-02 13:01:01 +0530

    Remove unused code and adding print statement.

--- cpp/next_Greater_Element/next_greater_element.cpp
+++ cpp/next_Greater_Element/next_greater_element.cpp
@@ -1,31 +1,5 @@
-// Aviral Gupta
 #include<bits/stdc++.h>
 using namespace std;
-const double PI = 3.14159265358979323846;
-#define LM 1e18
-#define ll  long long int
-#define ld long double
-#define ms(dp,val) memset(dp,val,sizeof(dp))
-#define all(t) t.begin(), t.end()
-#define inrange(i, a, b) ((i >= min(a, b)) && (i <= max(a, b)))
-#define pb push_back
-#define mp make_pair
-#define fi first
-#define se second 
-#define f(i,n) for(int i=0;i<n;i++)
-#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
-const ll mod=998244353;
-inline ll mul(ll a, ll b){ return (a * 1ll * b) % mod; }
-inline ll sub(ll a, ll b){ ll c = a - b; if(c < 0) c += mod; return c; }
-inline ll add(ll a, ll b){ ll c = a + b; if(c >= mod) c -= mod; return c; }
-
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector< pll > vpi;
-typedef vector<ll> vl;
-typedef vector<bool> vb;
-typedef vector<string> vs;
 
 void next_greater(vector<int> &A)
 {
@@ -51,13 +25,17 @@ void next_greater(vector<int> &A)
 
 int main()
 {
-    int n; cin>>n;
+    int n;
+    cout << "Enter the number of Elements: ";
+    cin>>n;
     vector<int> a(n);
+    cout << "\nEnter the Elements: ";
+
     for (int i = 0; i < n; i++)
     {
         cin>>a[i];
     }
 
     next_greater(a);
-    
-}
+
+} 
