f1b3b1e75e70aa2b454a2e430d75629db1a71cb4
Erick <viajero33176@innovaccion.mx>
2020-10-03 15:18:13 -0700

    Quick Sort

--- golang/Sorting Algorithms/Quick Sort/quick_sort.go
+++ golang/Sorting Algorithms/Quick Sort/quick_sort.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	// This is the input array to be sorted
 	testData := []int{846, 502, 4, 9, 38, 67, 109, 438, 650, 17, 4, 4, 4650, 1, 73, 45, 29, 0, 84, 54, 5}
 
 	fmt.Println("Sorted data:", quicksort(testData))
