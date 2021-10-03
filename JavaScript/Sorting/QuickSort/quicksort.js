8075224efd19abdef22fe6f53460252690c970d6
PRIYANSHU DAS <ask.priyanshu@gmail.com>
2021-10-03 16:57:29 +0530

    Edited the quicksort algorithm in javascript

--- JavaScript/Sorting/QuickSort/quicksort.js
+++ JavaScript/Sorting/QuickSort/quicksort.js
@@ -4,7 +4,7 @@ function quickSort(arr, start, end) {
         return arr;
 
     //pivot is the partitioning index
-    let pivot = partition(arr, start, end);
+    var pivot = partition(arr, start, end);
 
     // Separately sort elements before pivot and after pivot
     quickSort(arr, start, pivot - 1);
@@ -16,9 +16,9 @@ function quickSort(arr, start, end) {
 
 function partition(arr, p, r) {
 
-    let pivotValue = arr[r];
-    let pivotIndex = p;
-    for (let i = p; i < r; i++) {
+    var pivotValue = arr[r];
+    var pivotIndex = p;
+    for (var i = p; i < r; i++) {
         if (arr[i] < pivotValue) {
             swap(arr, i, pivotIndex);
             pivotIndex++;
@@ -31,13 +31,13 @@ function partition(arr, p, r) {
 
 
 function swap(arr, x, y) {
-    let temp = arr[x];
+    var temp = arr[x];
     arr[x] = arr[y];
     arr[y] = temp;
 
 }
 
 //testing the algorithm with an input
-let a = [4, 5, 2, 1, 6, 8]
-quickSort(a, 0, a.length - 1)
-console.log(a)
+var a = [4, 5, 2, 1, 6, 8];
+quickSort(a, 0, a.length - 1);
+console.log(a);
