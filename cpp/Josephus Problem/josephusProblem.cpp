b584a1551ba40dcf90d6e765dbf8acbc640a5dfa
Yash Seth <71393551+yash-seth@users.noreply.github.com>
2021-10-05 18:50:03 +0530

    Added Josephus Problem (#331)
    * Create josephusProblem.cpp
    
    * Create Josephus_Problem.txt

---
+++ cpp/Josephus Problem/josephusProblem.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int josephus(int n, int k)
+{
+	if (n == 1)
+		return 1;
+	else
+		/* The position returned by josephus(n - 1, k)
+		is adjusted because the recursive call
+		josephus(n - 1, k) considers the
+		original position k % n + 1 as position 1 */
+		return (josephus(n - 1, k) + k - 1) % n + 1;
+}
+
+// Driver Program to test above function
+//change the values of n and k as needed
+//to simulate the problem
+int main()
+{
+	int n = 14;
+	int k = 2;
+	cout << "The chosen place is " << josephus(n, k);
+	return 0;
+}
+
