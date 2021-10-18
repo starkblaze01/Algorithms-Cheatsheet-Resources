294f478e843d69b2ce0f5c0e9214261188ea1652
Korneliusz <korneliusz_krawczyk@o2.pl>
2021-10-18 10:27:19 +0200

    Updated is_prime.py

--- Python/is_prime.py
+++ Python/is_prime.py
@@ -1,16 +1,21 @@
 # Algorithm to check if a number is prime or not
 
-num = 331
-print("Is " + str(num) + " a prime number?")
+def main():
+    num = 311
+    print("Is " + str(num) + " a prime number?")
 
-prime = True
-if num > 1:
-    for i in range(2, num):
-        if (num % i) == 0:
-            prime = False
-            break
-    print(prime)
-elif num == 1:
-    print("1 is neither a prime nor a composite")
-else:
-    print("Give me natural number.")
+    prime = True
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                prime = False
+                break
+        print(prime)
+    elif num == 1:
+        print("1 is neither a prime nor a composite")
+    else:
+        print("Give me natural number.")
+
+
+if __name__ == "__main__":
+    main()
