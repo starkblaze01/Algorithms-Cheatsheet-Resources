b2297e8d31c26bc9897cb8e7cd24d7c6a9e7aa22
Mayank Pathela <mp.pathela@gmail.com>
2021-10-29 11:41:04 +0530

    Update Window_Sliding_Technique_To_find_max_sum_of_subarray_of_given_length.py

--- Python/Window_Sliding_Technique_To_find_max_sum_of_subarray_of_given_length.py
+++ Python/Window_Sliding_Technique_To_find_max_sum_of_subarray_of_given_length.py
@@ -3,11 +3,9 @@ def maxsum(arr,n,k):
         print("invalid")
         return -1
     window_sum=sum(arr[:k])
-    print(window_sum)
     max_sum=window_sum
     for i in range(n-k):
         window_sum=window_sum-arr[i]+arr[i+k]
-        print(window_sum)
         max_sum=max(max_sum,window_sum)
         
     return max_sum
